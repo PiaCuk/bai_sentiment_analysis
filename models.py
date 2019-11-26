5b34512f36581ab7fd2f9ae6f6212b85057b85bf
Pia Cuk <pia.cuk@gmx.de>
2019-11-26 18:30:20 +0100

    added two keras model and lstm model, find some results in train.py

--- models.py
+++ models.py
@@ -1,18 +1,59 @@
 from keras.models import Sequential
-from keras.layers import Input, Convolution1D, Dense, Flatten, Dropout
+from keras.layers import Conv1D, MaxPooling1D, LSTM, Dense, Flatten, Dropout
 
 def conv_model(input_shape, drop_rate=0.5, load_weights=None, verbose=True):
     model = Sequential()
     # Convolutional model (3x conv, flatten, 2x dense)
-    model.add(Convolution1D(64, 3, padding='same', input_shape=input_shape))
-    model.add(Convolution1D(32, 3, padding='same'))
-    model.add(Convolution1D(16, 3, padding='same'))
+    model.add(Conv1D(64, 3, padding='same', input_shape=input_shape))
+    model.add(Conv1D(32, 3, padding='same'))
+    model.add(Conv1D(16, 3, padding='same'))
     model.add(Flatten())
     model.add(Dropout(drop_rate))
     model.add(Dense(64, activation='sigmoid'))
     model.add(Dropout(drop_rate))
     model.add(Dense(5, activation='softmax'))
 
+    if load_weights is not None:
+        model.load_weights(load_weights)
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    if verbose:
+        print(model.summary())
+    return model
+
+def keras_conv_model(input_shape, load_weights=None, verbose=True):
+    model = Sequential()
+    
+    model.add(Conv1D(512, 3, activation='relu', padding='same', input_shape=input_shape))
+    model.add(MaxPooling1D(3, strides=2))
+    model.add(Conv1D(512, 3, activation='relu', padding='same'))
+    model.add(MaxPooling1D(3, strides=2))
+    model.add(Conv1D(512, 3, activation='relu', padding='same'))
+    model.add(MaxPooling1D(5))  # global max pooling
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(5, activation='softmax'))
+    
+    if load_weights is not None:
+        model.load_weights(load_weights)
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    if verbose:
+        print(model.summary())
+    return model
+
+def lstm_model(input_shape, drop_rate=0.5, load_weights=None, verbose=True):
+    model = Sequential()
+    model.add(LSTM(input_shape=input_shape, units=128, return_sequences=True))
+    model.add(Dropout(drop_rate))
+    model.add(LSTM(units=64))
+    model.add(Dropout(drop_rate))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(drop_rate))
+    model.add(Dense(5, activation='softmax'))
+    
     if load_weights is not None:
         model.load_weights(load_weights)
 
--- train.py
+++ train.py
@@ -2,17 +2,27 @@ import numpy as np
 from keras.utils import to_categorical
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 import matplotlib.pyplot as plt
-from models import conv_model
+from models import conv_model, keras_conv_model, lstm_model
 
 # elmo: 
-_SEQ_SHAPE = (25, 1024)
+#_SEQ_SHAPE = (25, 1024)
 # bert:
-#_SEQ_SHAPE = (25, 768)
+_SEQ_SHAPE = (25, 768)
 
 # specify embedding in test() and train() with parameter "embedding", default = bert
 def main():
-    #train(embedding='elmo')
-    test('models/weights.04-1.29.hdf5', embedding = 'elmo')
+    #train(embedding='bert')
+    test('models/weights.04-1.23.hdf5', embedding = 'bert')
+    '''
+    Accuracy: 46.21% for keras_128 and bert embedding
+    Accuracy: 47.18% for keras_256 and bert embedding
+    Accuracy: 46.07% for keras_512 and bert embedding
+    Accuracy: 47.56% for keras_512-256-128 and bert embedding
+    Accuracy: 45.84% for conv_model and bert
+    Accuracy: 48.30% for lstm_128 and bert embedding
+    Accuracy: 47.51% for lstm_64 and bert embedding
+    Accuracy: 48.67% for lstm_128-64 and bert embedding
+    '''
 
 ###############################################################################################################
 def train(plot=True, embedding = 'bert'):
@@ -21,7 +31,7 @@ def train(plot=True, embedding = 'bert'):
     x_val = np.load('data/x_dev_' + embedding + '.npy')
     y_val = np.load('data/y_dev_' + embedding + '.npy')
 
-    model = conv_model(_SEQ_SHAPE)
+    model = lstm_model(_SEQ_SHAPE)
     print(y_train.shape)
     print(x_train.shape)
 
@@ -46,7 +56,7 @@ def test(ckpt, embedding = 'elmo'):
     x_test = np.load('data/x_test_' + embedding + '.npy')
     y_test = np.load('data/y_test_' + embedding + '.npy')
 
-    model = conv_model(_SEQ_SHAPE, load_weights=ckpt)
+    model = lstm_model(_SEQ_SHAPE, load_weights=ckpt)
 
     scores = model.evaluate(x_test, y_test, verbose=0)
     print("Accuracy: %.2f%%" % (scores[1]*100))
